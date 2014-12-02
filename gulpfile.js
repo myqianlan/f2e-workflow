0cae526180217de5a87047ed620347c81d5695ea
myqianlan <linquantan@gmail.com>
2014-12-02 16:32:45 +0800

    任务依赖关系重构

--- gulpfile.js
+++ gulpfile.js
@@ -16,9 +16,11 @@ var minifycss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 // 自动加CSS浏览器前缀
 var autoprefixer = require('gulp-autoprefixer');
-//清除
+// 图片压缩
+var imagemin = require('gulp-imagemin');
+// 清除
 var rimraf = require('gulp-rimraf');
-
+// 输出美化
 var chalk = require('chalk');
 
 
@@ -41,7 +43,6 @@ gulp.task('server', function() {
     http.createServer(app).listen(port);
     open(protocol + '://' + host + ':' + port + '/index.html');
 });
-
 // clean css files
 gulp.task('cleancss', function() {
     return gulp.src(root + '/css', {
@@ -50,21 +51,13 @@ gulp.task('cleancss', function() {
         .pipe(rimraf());
 });
 
-// Compile SASS  
-// with node sass
-gulp.task('sass', ['cleancss'], function() {
+// Compile SASS 
+gulp.task('css', ['cleancss'], function() {
     return gulp.src(root + '/scss/**/*.scss')
         .pipe(sass({
             errLogToConsole: true
         }))
-        .pipe(gulp.dest(root + '/css'));
-});
-
-// 自动添加浏览器前缀
-// By default, Autoprefixer uses > 1%, last 2 versions, Firefox ESR, Opera 12.1
-gulp.task('autoprefixer', function() {
-    return gulp.src(root + '/css/**/*.css')
-        .pipe(autoprefixer())
+        .pipe(autoprefixer())// By default, Autoprefixer uses > 1%, last 2 versions, Firefox ESR, Opera 12.1
         .pipe(gulp.dest(root + '/css'));
 });
 
@@ -73,49 +66,74 @@ gulp.task('autoprefixer', function() {
 gulp.task('minifyjs', function() {
     return gulp.src(root + '/js/**/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest(root + '/temp'));
-});
-
-// 压缩CSS文件   
-gulp.task('minifycss', function() {
-    return gulp.src(root + '/css/**/*.css')
-        .pipe(minifycss())
-        .pipe(gulp.dest(root + '/temp'));
-});
-
-// 重命名，加入时间戳版本号什么的
-gulp.task('version', function() {
-    return gulp.src(root + '/temp/**/*.*')
         .pipe(rename({
             suffix: '.min-' + new Date().getTime()
         }))
-        .pipe(gulp.dest(root + '/dist'));
+        .pipe(gulp.dest(root +'/_temp/js'));
+});
+
+// 压缩CSS文件   
+gulp.task('minifycss', ['css'],function() {
+    return gulp.src(root + '/css/**/*.css')
+        .pipe(minifycss())
+        .pipe(rename({
+            suffix: '.min-' + new Date().getTime()
+        }))
+        .pipe(gulp.dest(root +'/_temp/css'));
+});
+
+// 压缩 static images
+gulp.task('imagemin', function() {
+ return gulp.src(root + '/img/**/*')
+    // Pass in options to the task
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest(root + '/_temp/img'));
+});
+
+// 复制文件
+gulp.task('copy', ['minifyjs','minifycss','imagemin','copyvendor'], function() {
+    return gulp.src(root + '/_temp/**/*')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copyvendor', function() {
+    return gulp.src(root + '/vendor/**/*')
+        .pipe(gulp.dest(root + '/_temp/vendor'));
 });
 
 // 使用说明
 gulp.task('help', function() {
-    console.log(  chalk.white.bgCyan.bold('\n\nF2E-workflow使用说明：\n\n gulp && gulp help：帮助信息\n gulp dev：不带本地服务器的开发\n gulp sdev：带本地服务器的开发\n gulp build：压缩JS CSS代码，并且加时间戳\n\n更多详细的请配置gulpfile.js文件\n\n' )  );
+    console.log(  chalk.white.bgCyan.bold('\n\nF2E-workflow使用说明：\n\n gulp && gulp help：帮助信息\n gulp dev：不带本地服务器的开发\n gulp sdev：带本地服务器的开发\n gulp build：压缩JS CSS代码，并且加时间戳,压缩图片\n gulp cleanbuild：删除dist文件夹\n\n更多详细的请配置gulpfile.js文件\n\n' )  );
 });
+
+
 
 // 默认任务   
 gulp.task('default', ['help']    
 );
 
 // 开发任务
-gulp.task('sdev', ['sass', 'autoprefixer', 'server'], function() {
+gulp.task('sdev', ['css', 'server'], function() {
 
     // 监视scss文件的变化,并且执行sass && autoprefixer
-    gulp.watch(root + '/scss/**/*.scss', ['sass', 'autoprefixer']);
+    gulp.watch(root + '/scss/**/*.scss', ['css']);
 });
 // 不带本地服务器
-gulp.task('dev', ['sass', 'autoprefixer'], function() {
+gulp.task('dev', ['css'], function() {
     // 监视scss文件的变化,并且执行sass && autoprefixer
-    gulp.watch(root + '/scss/**/*.scss', ['sass', 'autoprefixer']);
+    gulp.watch(root + '/scss/**/*.scss', ['css']);
 });
 
 // 构建任务
-gulp.task('build', ['minifyjs', 'sass', 'autoprefixer', 'minifycss', 'version'], function() {
-    gulp.src(root + '/temp', {
+gulp.task('build', ['copy'], function() {
+    return gulp.src(root + '/_temp', {
+            read: false
+        }) // much faster
+        .pipe(rimraf());
+});
+// clean build
+gulp.task('cleanbuild', function() {
+    return gulp.src('./dist', {
             read: false
         }) // much faster
         .pipe(rimraf());
