12df08cba417e403b78aa2d75134fbb25f085a01
myqianlan <linquantan@gmail.com>
2015-09-18 18:22:36 +0800

    autoprefixer browser config 修正

--- gulpfile.js/config/sass.js
+++ gulpfile.js/config/sass.js
@@ -1,7 +1,7 @@
 var config = require("./");
 
 module.exports = {
-    autoprefixer: {browser: ["lst 3 version", ">1% in CN", "ie >= 9", "ie_mob >= 10","Android >= 4"]},
+    autoprefixer: {browser: ["last 3 version", "> 1% in CN", "ie >= 9", "ie_mob >= 10","Android >= 4"]},
     src: config.sourceAssets + "/scss/**/*.scss",
     dest: config.sourceAssets + "/css",
     settings: {
